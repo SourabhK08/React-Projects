b804cfd2fbe0feab01f560d1561edab238c5bd5a
Sourabh_kakani <kakanisourabh23@gmail.com>
2023-10-24 13:50:49 +0530

    added router

--- 06-react-router/src/App.jsx
+++ 06-react-router/src/App.jsx
@@ -2,11 +2,19 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Outlet />
+      {/* dynamically we chg here routes by making header and footer constant */}
+      <Footer />
+    </>
+  );
 }
 
 export default App;
