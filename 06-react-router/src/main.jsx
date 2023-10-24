68268dc101481af80cdcd9f314af25f0d188e60b
Sourabh_kakani <kakanisourabh23@gmail.com>
2023-10-24 14:07:02 +0530

    created about us page

--- 06-react-router/src/main.jsx
+++ 06-react-router/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./components/home/Home.jsx";
+import About from "./components/about/About.jsx";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
       {
         path: "",
         element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
       },
     ],
   },
