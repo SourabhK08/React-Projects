5c2d014d85e5d02cd899fb918022f9d7efe7acf4
Sourabh_kakani <kakanisourabh23@gmail.com>
2023-10-24 14:17:18 +0530

    created contact us page

--- 06-react-router/src/main.jsx
+++ 06-react-router/src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./components/home/Home.jsx";
 import About from "./components/about/About.jsx";
+import Contact from "./components/contact/Contact.jsx";
 
 const router = createBrowserRouter([
   {
@@ -18,6 +19,10 @@ const router = createBrowserRouter([
       {
         path: "about",
         element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
       },
     ],
   },
