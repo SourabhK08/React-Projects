845221732df7d8125e52f5645c62b2ae27590c15
Sourabh_kakani <kakanisourabh23@gmail.com>
2023-10-29 09:34:51 +0530

    mini context api

--- 07-mini-context/src/context/UserContextProvider.jsx
+++ 07-mini-context/src/context/UserContextProvider.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import UserContext from "./UserContext";
 
-function UserContextProvider({ childern }) {
-  const [user, setUser] = useState(null);
+const UserContextProvider = ({ children }) => {
+  const [user, setUser] = React.useState(null);
   return (
     <UserContext.Provider value={{ user, setUser }}>
-      {childern}
+      {children}
     </UserContext.Provider>
   );
-}
+};
 
 export default UserContextProvider;
