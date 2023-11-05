44cac1050102c648bc3d5705ee00952676dd670e
Sourabh_kakani <kakanisourabh23@gmail.com>
2023-11-05 16:08:30 +0530

    add todo

--- 10-todo-localStorage/src/App.jsx
+++ 10-todo-localStorage/src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
 import "./App.css";
+import { TodoProvider } from "./context/TodoContext";
 
 function App() {
   const [todos, setTodos] = useState([]);
 
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+  };
+
   return (
-    <>
+    <TodoProvider
+      value={{ todos, addTodo, deleteTodo, updateTodo, toggleCompleted }}
+    >
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
@@ -17,7 +24,7 @@ function App() {
           </div>
         </div>
       </div>
-    </>
+    </TodoProvider>
   );
 }
 
